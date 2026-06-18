c750b48d97ce3ee0d85b595b1b6c744943b1a77b
smysak <98991422+smysak@users.noreply.github.com>
2026-06-18 16:55:20 +0800

    create method to get player choice

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,15 +7,38 @@ class GameBoard
 
   def initialize
     @grid_values = [nil, *1..9]
+    # change top, mid, bot to class methods
     @top = " #{@grid_values[1]} | #{@grid_values[2]} | #{@grid_values[3]} "
     @mid = " #{@grid_values[4]} | #{@grid_values[5]} | #{@grid_values[6]} "
     @bot = " #{@grid_values[7]} | #{@grid_values[8]} | #{@grid_values[9]} "
   end
 end
 
+# Cut this down with other methods
+def choose(game)
+  puts "Choose where #{game.grid_values[0]} will play next."
+  loop do
+    input = gets.chomp
+    if input.match?(/^[1-9]$/)
+      index = input.to_i
+      if game.grid_values[index].is_a?(Integer)
+        return index
+      else
+        puts "That space is already taken by #{game.grid_values[index]}.\nTry again."
+      end
+    else
+      puts "I have no idea where #{input} is.\nTry again."
+    end
+  end
+end
+
 def play_game
   game = GameBoard.new
+  # 9 times loop?
   puts "#{game.top}\n-----------\n#{game.mid}\n-----------\n#{game.bot}"
+  # implement player switch
+  game.grid_values[0] = "X"
+  game.grid_values[choose(game)] = game.grid_values[0]
 end
 
 play_game
